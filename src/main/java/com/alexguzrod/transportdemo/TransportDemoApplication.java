9b200472ee9ec8d5bca97f765f92b4176bac01f7
Alexander Guzman <alexguzman1227@gmail.com>
2022-07-11 07:00:02 -0500

    Feat: Create project

---
+++ src/main/java/com/alexguzrod/transportdemo/TransportDemoApplication.java
@@ -0,0 +1,13 @@
+package com.alexguzrod.transportdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportDemoApplication.class, args);
+	}
+
+}
